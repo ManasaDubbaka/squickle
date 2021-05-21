a9c7cb9e821ac20c0eb31bb860d16a49fcb8983d
ManasaDubbaka <manasa.dubbaka19@gmail.com>
2021-05-21 14:13:51 +0530

    app bar

---
+++ my-sq/src/App.js
@@ -0,0 +1,13 @@
+import PrimarySearchAppBar from './components/appbar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <PrimarySearchAppBar/>
+    
+    </div>
+  );
+}
+
+export default App;
